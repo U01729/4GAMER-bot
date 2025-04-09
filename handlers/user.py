ea7a32e8bc090df87e601681fcb4b1b571e3f41f
RaenTC9_91 <pubgmobilekr862@gmail.com>
2025-04-09 21:59:05 +0300

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,1 +1,12 @@
+# handlers/user.py
+from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+async def start_cmd(message: types.Message):
+    # عند إرسال أمر /start
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("ألعاب PS4"))
+    await message.answer("مرحبًا بك في 4GAMER بوت!", reply_markup=keyboard)
+
+def setup(dp):
+    dp.register_message_handler(start_cmd, commands=['start'])
