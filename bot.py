83654392053d879dd73f81a72cf17f0c26c2e883
RaenTC9_91 <pubgmobilekr862@gmail.com>
2025-04-09 21:55:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,59 +1,24 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+# bot.py
+from aiogram import Bot, Dispatcher
+from aiogram.types import ParseMode
 from aiogram.utils import executor
-from config import BOT_TOKEN, ADMIN_ID
+import logging
+from config import BOT_TOKEN
 
-# إعداد البوت
+# تكوين تسجيل الدخول
+logging.basicConfig(level=logging.INFO)
+
+# تعريف البوت والموزع (dispatcher)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# قائمة الألعاب
-games = []
+# استيراد ملفات المعالجات بعد تعريف dp لتجنب الاستيراد الدائري
+from handlers import user, admin
 
-# قائمة لحفظ المستخدمين الذين تفاعلوا مع البوت
-users = set()
+# إعداد الأوامر باستخدام setup
+user.setup(dp)
+admin.setup(dp)
 
-@dp.message_handler(commands=['start'])
-async def start_cmd(message: types.Message):
-    users.add(message.from_user.id)
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("ألعاب PS4"))
-    await message.answer("مرحباً بك في 4GAMER بوت!", reply_markup=keyboard)
-
-@dp.message_handler(commands=['broadcast'])
-async def broadcast(message: types.Message):
-    if message.from_user.id != ADMIN_ID:
-        return await message.answer("أنت غير مصرح لك باستخدام هذا الأمر.")
-    await message.answer("أرسل الآن الرسالة التي تريد إرسالها للمستخدمين.")
-    
-    @dp.message_handler()
-    async def get_broadcast(msg: types.Message):
-        for user_id in users:
-            try:
-                await bot.send_message(user_id, msg.text)
-            except:
-                continue
-        await msg.answer("تم إرسال الرسالة بنجاح.")
-
-@dp.message_handler(commands=['addgame'])
-async def add_game(message: types.Message):
-    if message.from_user.id != ADMIN_ID:
-        return await message.answer("أمر غير مسموح لك.")
-    
-    await message.answer("أرسل اسم اللعبة مع الرابط بالشكل التالي:\n\nاسم اللعبة - الرابط")
-    
-    @dp.message_handler()
-    async def save_game(msg: types.Message):
-        games.append(msg.text)
-        await msg.answer("تمت إضافة اللعبة!")
-
-@dp.message_handler(lambda message: message.text == "ألعاب PS4")
-async def show_games(message: types.Message):
-    if not games:
-        await message.answer("لا توجد ألعاب مضافة بعد.")
-    else:
-        reply = "\n\n".join(games)
-        await message.answer(f"قائمة الألعاب:\n\n{reply}")
-
-if __name__ == '__main__':
+# تشغيل البوت باستخدام polling
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
